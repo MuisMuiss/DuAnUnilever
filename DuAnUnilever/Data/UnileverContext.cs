735aca966644332c0dc74a67abd0acefce280c0c
Tran Binh <0306201006@caothang.edu.vn>
2023-04-10 11:26:08 +0700

    first commit

---
+++ DuAnUnilever/Data/UnileverContext.cs
@@ -0,0 +1,14 @@
+﻿using DuAnUnilever.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuAnUnilever.Data
+{
+    public class UnileverContext : DbContext
+    {
+        public UnileverContext(DbContextOptions<UnileverContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ DuAnUnilever/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DuAnUnilever.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [DisplayName("Tên đăng nhập")]
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} từ 6-20 kí tự")]
+        public string Username { get; set; }
+
+        [DisplayName("Mật khẩu")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "{0} từ 6-100 kí tự")]
+        public string Password { get; set; }
+        
+        [DisplayName("Full name")]
+        public string FullName { get; set; }
+
+        [DisplayName("Email")]
+        [EmailAddress(ErrorMessage = "{0} không hợp lệ")]
+        public string Email { get; set; }
+
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [DisplayName("Area")]
+        public string Area { get; set; }
+
+        [DisplayName("Report to")]
+        public string Report { get; set; }
+
+        [DisplayName("Trạng thái")]
+        [DefaultValue(true)]
+        public bool Status { get; set; } = true;
+
+    }
+}
